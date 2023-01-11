3461aab1606c68d48960a268e0aa28f4560631ac
Madhav Solanki <madhav09solanki@gmail.com>
2023-01-11 22:11:33 +0530

    This is my CryptoCurrency tracking System

---
+++ app/src/main/java/com/example/cryptotracker/CurrencyRVAdapter.java
@@ -0,0 +1,61 @@
+package com.example.cryptotracker;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class CurrencyRVAdapter extends RecyclerView.Adapter<CurrencyRVAdapter.ViewHolder> {
+    private ArrayList<CurrencyRVModel> currencyRVModelArrayList ;
+    private Context context;
+    private static DecimalFormat df2 = new DecimalFormat("#.##");
+
+    public CurrencyRVAdapter(ArrayList<CurrencyRVModel> currencyRVModelArrayList, Context context) {
+        this.currencyRVModelArrayList = currencyRVModelArrayList;
+        this.context = context;
+    }
+
+  @SuppressLint("NotifyDataSetChanged")
+  public void filterList(ArrayList<CurrencyRVModel> filteredList){
+        currencyRVModelArrayList = filteredList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public CurrencyRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      View view  = LayoutInflater.from(context).inflate(R.layout.currency_rv_item ,parent,false);
+      return  new CurrencyRVAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CurrencyRVAdapter.ViewHolder holder, int position) {
+        CurrencyRVModel currencyRVModel = currencyRVModelArrayList.get(position);
+        holder.currencyNameTV.setText(currencyRVModel.getName());
+        holder.symbolTV.setText(currencyRVModel.getSymbol());
+        holder.rateTV.setText("$"+df2.format(currencyRVModel.getPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return currencyRVModelArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+            private TextView currencyNameTV, symbolTV, rateTV ;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            currencyNameTV = itemView.findViewById(R.id.idTVCurrencyName);
+            symbolTV = itemView.findViewById(R.id.idTVSymbol);
+            rateTV = itemView.findViewById(R.id.idTVCurrencyRate);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cryptotracker/MainActivity.java
@@ -0,0 +1,135 @@
+package com.example.cryptotracker;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.jar.JarException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText searchEdt;
+    private RecyclerView currenciesRV ;
+    private ProgressBar loadingPB ;
+    private ArrayList<CurrencyRVModel> currencyRVModelArrayList;
+    private CurrencyRVAdapter currencyRVAdapter ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchEdt = findViewById(R.id.idEdtSearch);
+        currenciesRV = findViewById(R.id.idRVCurrencies);
+        loadingPB = findViewById(R.id.idPBLoading);
+         currencyRVModelArrayList = new ArrayList<>();
+         currencyRVAdapter = new CurrencyRVAdapter(currencyRVModelArrayList,this);
+         currenciesRV.setLayoutManager(new LinearLayoutManager(this));
+         currenciesRV.setAdapter(currencyRVAdapter);
+         getCurrencyData();
+
+         searchEdt.addTextChangedListener(new TextWatcher() {
+             @Override
+             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+             }
+
+             @Override
+             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+             }
+
+             @Override
+             public void afterTextChanged(Editable s) {
+                    filterCurrencies(s.toString());
+             }
+         });
+    }
+
+    private void filterCurrencies(String currency){
+        ArrayList<CurrencyRVModel> filteredList = new ArrayList<>();
+        for (CurrencyRVModel item: currencyRVModelArrayList){
+            if (item.getName().toLowerCase().contains(currency.toLowerCase())){
+                filteredList.add(item);
+            }
+        }
+        if (filteredList.isEmpty()){
+            Toast.makeText(this, "No Currency Found for searched query", Toast.LENGTH_SHORT).show();
+        }else {
+            currencyRVAdapter.filterList(filteredList);
+        }
+    }
+
+    private void getCurrencyData(){
+        loadingPB.setVisibility(View.VISIBLE);
+            String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest" ;
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            public void onResponse(JSONObject response) {
+                loadingPB.setVisibility(View.GONE);
+                try {
+                    JSONArray dataArray = response.getJSONArray("data");
+                    for (int i = 0; i < dataArray.length() ; i++) {
+                        JSONObject dataObj = dataArray.getJSONObject(i);
+                        String name = dataObj.getString("name");
+                        String symbol = dataObj.getString("symbol");
+                        JSONObject quote = dataObj.getJSONObject("quote");
+                        JSONObject USD = quote.getJSONObject("USD");
+                        double price  = USD.getDouble("price");
+                        currencyRVModelArrayList.add(new CurrencyRVModel(name,symbol,price));
+                    }
+                    currencyRVAdapter.notifyDataSetChanged();
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Fail to extract JSON data........", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                loadingPB.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Sorry to Fail to get the data....", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String,String> headers = new HashMap<>();
+                headers.put("X-CMC_PRO_API_KEY","804789db-4cb4-41a5-ad67-e3ee837a231e");
+                return headers;
+            }
+        };
+
+        requestQueue.add(jsonObjectRequest);
+    }
+}
